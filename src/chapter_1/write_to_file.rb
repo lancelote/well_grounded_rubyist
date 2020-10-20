e07c63b31fe939cd021f0e2ed54f2a47077a3c8b
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-10-20 21:07:22 +0300

    Fix file path to write

--- src/chapter_1/write_to_file.rb
+++ src/chapter_1/write_to_file.rb
@@ -2,6 +2,6 @@ print "hello. please enter a celsius value: "
 celsius = gets.to_i
 fahrenheit = (celsius * 9 / 5) + 32
 puts "savings result to output file 'temp.out'"
-fh = File.new("temp.out", "w")
+fh = File.new("data/temp.out", "w")
 fh.puts fahrenheit
 fh.close
